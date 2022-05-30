cb44051c66d8570360048830d901d4b8ede2290c
dyang39 <dyang39@ucsc.edu>
2022-05-30 15:53:14 -0700

    create postprocessing

---
+++ postprocessing.py
@@ -0,0 +1,20 @@
+import re
+
+def postprocessing(ds):
+  '''
+  input ds: dialog state for the current turn as string
+  return prediction: valued prediction as list of triplet
+  '''
+  
+  current_belief = re.search('<\|belief\|> (.*) <\|endofbelief\|>', ds)
+  current_belief = current_belief.group(1)
+
+  belief_list = re.split(', ', current_belief)  # here can split by ", domain", using commas alone is not enough
+
+  prediction = []
+  for x in result_list:
+    not_mentioned = re.search('not mentioned', x)
+    if not_mentioned == None:
+      precition.append(x)
+  
+  return prediction
